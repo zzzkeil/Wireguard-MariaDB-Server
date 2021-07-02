d4e190ba981fe119f9274ccc32b1be6934b0c624
Klaus <38099918+zzzkeil@users.noreply.github.com>
2021-07-03 00:42:52 +0200

    Update test_x86.sh

--- debian_ubuntu/test_x86.sh
+++ debian_ubuntu/test_x86.sh
@@ -63,7 +63,11 @@ mkdir /root/script_backupfiles/
 ### wireguard port stettings
 echo " Make your port settings now:"
 echo "------------------------------------------------------------"
-read -p "Choose your Wireguard Port: " -e -i 51820 wg0port
+read -p "Choose your Wireguard Port: " -e -i 51822 wg0port
+echo "------------------------------------------------------------"
+echo
+echo "------------------------------------------------------------"
+read -p "Choose your MariaDB Port: " -e -i 3306 dbport
 echo "------------------------------------------------------------"
 #
 ### apt systemupdate and installs	 
@@ -161,23 +165,23 @@ sed -i "s@IP01@$(hostname -I | awk '{print $1}')@" /etc/wireguard/client2.conf
 chmod 600 /etc/wireguard/client2.conf
 
 
-apt install mariadb-server 
+apt install mariadb-server -y
 /usr/sbin/service mysql stop
 ###configure MariaDB
 
 ### MariaDB Data-at-Rest Encryption
-#mkdir /etc/mysql/keys
-#echo  "1;"$(openssl rand -hex 32) > /etc/mysql/keys/enc_keys
-#echo  "2;"$(openssl rand -hex 32) >> /etc/mysql/keys/enc_keys
-#echo  "3;"$(openssl rand -hex 32) >> /etc/mysql/keys/enc_keys
-#echo  "4;"$(openssl rand -hex 32) >> /etc/mysql/keys/enc_keys
-#openssl rand -hex 256 > /etc/mysql/keys/enc_paswd.key
-#openssl enc -aes-256-cbc -md sha1 -pbkdf2 -pass file:/etc/mysql/keys/enc_paswd.key -in /etc/mysql/keys/enc_keys -out /etc/mysql/keys/enc_key.enc
+mkdir /etc/mysql/keys
+echo  "1;"$(openssl rand -hex 32) > /etc/mysql/keys/enc_keys
+echo  "2;"$(openssl rand -hex 32) >> /etc/mysql/keys/enc_keys
+echo  "3;"$(openssl rand -hex 32) >> /etc/mysql/keys/enc_keys
+echo  "4;"$(openssl rand -hex 32) >> /etc/mysql/keys/enc_keys
+openssl rand -hex 256 > /etc/mysql/keys/enc_paswd.key
+openssl enc -aes-256-cbc -md sha1 -pbkdf2 -pass file:/etc/mysql/keys/enc_paswd.key -in /etc/mysql/keys/enc_keys -out /etc/mysql/keys/enc_key.enc
 
-#chown -R mysql:root /etc/mysql/keys
-#chmod 500 /etc/mysql/keys/
-#chown mysql:root /etc/mysql/keys/enc_paswd.key /etc/mysql/keys/enc_key.enc
-#chmod 600 /etc/mysql/keys/enc_paswd.key /etc/mysql/keys/enc_key.enc
+chown -R mysql:root /etc/mysql/keys
+chmod 500 /etc/mysql/keys/
+chown mysql:root /etc/mysql/keys/enc_paswd.key /etc/mysql/keys/enc_key.enc
+chmod 600 /etc/mysql/keys/enc_paswd.key /etc/mysql/keys/enc_key.enc
 
 
 ### MariaDB Data-in-Transit Encryption
@@ -193,105 +197,41 @@ apt install mariadb-server
 #chown mysql.mysql /etc/mysql/certs/
 
 
-#echo "
-#[sqld]
+echo "
+[sqld]
 
-#File Key Management Plugin
-#plugin_load_add=file_key_management
-#file_key_management = ON file_key_management_encryption_algorithm=aes_cbc file_key_management_filename = /etc/mysql/keys/enc_keys.enc
-#file_key_management_filekey = /etc/mysql/keys/enc_paswd.key
+# File Key Management Plugin
+plugin_load_add=file_key_management
+file_key_management = ON file_key_management_encryption_algorithm=aes_cbc file_key_management_filename = /etc/mysql/keys/enc_keys.enc
+file_key_management_filekey = /etc/mysql/keys/enc_paswd.key
 
 # InnoDB/XtraDB Encryption Setup
-#innodb_default_encryption_key_id = 1
-#innodb_encrypt_tables = ON
-#innodb_encrypt_log = ON
-#innodb_encryption_threads = 4
+innodb_default_encryption_key_id = 1
+innodb_encrypt_tables = ON
+innodb_encrypt_log = ON
+innodb_encryption_threads = 4
 
 # Aria Encryption Setup
-#aria_encrypt_tables = ON
+aria_encrypt_tables = ON
 
 # Temp & Log Encryption
-#encrypt-tmp-disk-tables = 1
-#encrypt-tmp-files = 1
-#encrypt_binlog = ON
-#" > /etc/mysql/my_enc.cnf
+encrypt-tmp-disk-tables = 1
+encrypt-tmp-files = 1
+encrypt_binlog = ON
+" > /etc/mysql/my_enc.cnf
 
 
 mv /etc/mysql/my.cnf /etc/mysql/my.cnf.bak
-echo "[client]
-default-character-set = utf8mb4
-port = 3306
-[mysqld_safe]
-log_error=/var/log/mysql/mysql_error.log
-nice = 0
-socket = /var/run/mysqld/mysqld.sock
-[mysqld]
-basedir = /usr
+echo "
 bind-address = 0.0.0.0
-binlog_format = ROW
-bulk_insert_buffer_size = 16M
-character-set-server = utf8mb4
-collation-server = utf8mb4_general_ci
-concurrent_insert = 2
-connect_timeout = 5
-datadir = /var/lib/mysql
-default_storage_engine = InnoDB
-expire_logs_days = 10
-general_log_file = /var/log/mysql/mysql.log
-general_log = 0
-innodb_buffer_pool_size = 1024M
-innodb_buffer_pool_instances = 1
-innodb_flush_log_at_trx_commit = 2
-innodb_log_buffer_size = 32M
-innodb_max_dirty_pages_pct = 90
-innodb_file_per_table = 1
-innodb_open_files = 400
-innodb_io_capacity = 4000
-innodb_flush_method = O_DIRECT
-key_buffer_size = 128M
-lc_messages_dir = /usr/share/mysql
-lc_messages = en_US
-log_bin = /var/log/mysql/mariadb-bin
-log_bin_index = /var/log/mysql/mariadb-bin.index
-log_error = /var/log/mysql/mysql_error.log
-log_slow_verbosity = query_plan
-log_warnings = 2
-long_query_time = 1
-max_allowed_packet = 16M
-max_binlog_size = 100M
-max_connections = 200
-max_heap_table_size = 64M
-myisam_recover_options = BACKUP
-myisam_sort_buffer_size = 512M
-port = 3306
-pid-file = /var/run/mysqld/mysqld.pid
-query_cache_limit = 2M
-query_cache_size = 64M
-query_cache_type = 1
-query_cache_min_res_unit = 2k
-read_buffer_size = 2M
-read_rnd_buffer_size = 1M
-skip-external-locking
-skip-name-resolve
-slow_query_log_file = /var/log/mysql/mariadb-slow.log
-slow-query-log = 1
-socket = /var/run/mysqld/mysqld.sock
-sort_buffer_size = 4M
-table_open_cache = 400
-thread_cache_size = 128
-tmp_table_size = 64M
-tmpdir = /tmp
-transaction_isolation = READ-COMMITTED
-user = mysql
-wait_timeout = 600
+port = $dbport
 
-[mysqldump]
-max_allowed_packet = 16M
-quick
-quote-names
+slow_query_log_file    = /var/log/mysql/mariadb-slow.log
+long_query_time        = 10
+log_slow_rate_limit    = 1000
+log_slow_verbosity     = query_plan
+log-queries-not-using-indexes
 
-[isamchk]
-key_buffer = 16M
 " > /etc/mysql/my.cnf
 /usr/sbin/service mysql restart
 clear
@@ -302,20 +242,16 @@ echo ""
 mysql_secure_installation
 
 
-
-
-
-
-
+systemctl enable wg-quick@wg0.service
+systemctl start wg-quick@wg0.service
 wget -O  add_database.sh https://raw.githubusercontent.com/zzzkeil/Wireguard-MariaDB-Server/main/add_database.sh
 chmod +x add_database.sh
 curl -o add_client.sh https://raw.githubusercontent.com/zzzkeil/Wireguard-DNScrypt-VPN-Server/master/tools/add_client.sh
 curl -o remove_client.sh https://raw.githubusercontent.com/zzzkeil/Wireguard-DNScrypt-VPN-Server/master/tools/remove_client.sh
 chmod +x add_client.sh
 chmod +x remove_client.sh
-
+clear
 echo " to add or remove clients run ./add_client.sh or remove_client.sh"
 echo " To add a database run ./add_database.sh "
 
-systemctl enable wg-quick@wg0.service
-systemctl start wg-quick@wg0.service
+
